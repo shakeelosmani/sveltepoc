2dd31bf41210590499df420602b69456c89f1acf
Shakeel Osmani <shakeel.osmani@amerisave.com>
2021-10-15 01:35:22 -0400

    poc done

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let name: string = "todos";    
+</script>
+
+<p>Go to <a href="/{name}">{name}</a></p>
+
+
+
---
+++ src/routes/todos.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import type { Todo } from "src/models/todo";
+    import { onMount } from 'svelte'
+    
+    export let todos: Array<Todo> = [];
+
+
+    onMount(async () => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+        todos = await res.json();
+        //window.alert("loaded everything... message using window")
+    })     
+</script>
+
+<ul>
+    {#each todos as todo}
+        <li>{todo.id}</li>
+        <li>{todo.title}</li>
+        <li>{todo.userId}</li>
+        <li>{todo.completed}</li>
+    {/each}
+</ul>
+<br>
+<p>Go back to <a href="/">Home</a></p>
+
+<style lang="scss">
+    ul {
+        list-style: none;
+    }
+</style>
